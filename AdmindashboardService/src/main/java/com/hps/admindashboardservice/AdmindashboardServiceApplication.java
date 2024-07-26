d16ab8e1030c997af508341ce112f2647390a2ef
Yahya Saadaoui <saadaouiyahya706@gmail.com>
2024-07-26 01:47:57 +0100

    State All working Except the gatway

---
+++ AdmindashboardService/src/main/java/com/hps/admindashboardservice/AdmindashboardServiceApplication.java
@@ -0,0 +1,14 @@
+package com.hps.admindashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class AdmindashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmindashboardServiceApplication.class, args);
+    }
+
+}
---
+++ MerchantServicingService/src/main/java/com/hps/merchantservicingservice/MerchantServicingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hps.merchantservicingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantServicingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantServicingServiceApplication.class, args);
+    }
+
+}
